9561b66923e43d8f289049f06a86c00dd6fb8d91
Maksym Snigirov <maksym.snigirov@craftablesoftware.com>
2021-07-22 23:12:13 +0100

    first try

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,87 @@
 <script>
-	export let name;
+	import Button from './Button.svelte';
+	import Product from "./Product.svelte";
+	import Cart from "./Cart.svelte";
+
+	let title = '';
+	let price = '0';
+	let description = '';
+
+	let products = [];
+	let cartItems = [];
+
+	const setTitle =(e) => {
+		title = e.target.value;
+	}
+
+	const createProduct =(e) => {
+		const newProduct = {
+			title,
+			price,
+			description,
+		}
+		products = products.concat(newProduct);
+	}
+
+	const addToCart =(e) => {
+		const selectedTitle = e.detail;
+		
+		cartItems = cartItems.concat({
+			...products.find(prod=> prod.title === selectedTitle)
+		});
+		console.log(cartItems)
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<section>
+	<Cart items={cartItems} />
+	<hr/>
+</section>
+
+<section>
+	<div>
+		<label for='title'>Title</label>
+		<input type="text" id='title' value="{title}" on:input="{setTitle}" />
+	</div>
+	<div>
+		<label for='prce'>Price</label>
+		<input type="number" id="price" bind:value={price} />
+	</div>
+	<div>
+		<label for='description'>Description</label>
+		<textarea rows="3" id="description" bind:value={description} />
+	</div>
+	<Button on:click={createProduct}>Submit</Button>
+</section>
+<section>
+	{#if products.length===0}
+		<p>No product were added yet!</p>
+	{:else}
+		{#each products as product}
+			<Product  
+				productTitle={product.title}
+				productPrice={product.price}
+				productDescription={product.description}
+				on:addcart={addToCart}
+			/>
+		{/each}
+	{/if}
+</section>
+
 
 <style>
+	section{
+		width: 30rem;
+		margin: auto;
+
+	}
+
+	label, 
+	textarea, 
+	input{
+		width: 100%;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
---
+++ src/Button.svelte
@@ -0,0 +1,19 @@
+<style>
+	
+	button {
+		font: inherit;
+		padding: 0.15rem 0.5rem;
+		background-color: lightcoral;
+		color: white;
+		cursor: pointer;
+	}
+
+	button:hover,
+	button:active{
+		box-shadow:rgba(1px, 1px, 6px, 0.26)
+	}
+</style>
+
+<button on:click>
+	<slot/>
+</button>
